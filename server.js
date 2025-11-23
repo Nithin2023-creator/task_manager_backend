e1b647f94ae3467895251e89926ff6da52f766c0
Nithin Chowdary <nithinchowdary2354@gmail.com>
2025-11-23 20:43:43 +0530

    init

--- server.js
+++ server.js
@@ -10,10 +10,11 @@ const app = express();
 
 // Middleware
 app.use(cors());
+
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/productivi_flow', {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => console.log('MongoDB Connected'))
